d7b6f204f762039309a9f34388f5af2103d8082f
urdaeng <gni711@naver.com>
2024-08-27 17:05:26 +0900

    Feat: 마이페이지 상단 UI

--- WeClimb/View/MyPageVC.swift
+++ WeClimb/View/MyPageVC.swift
@@ -6,24 +6,178 @@
 //
 
 import UIKit
+import SnapKit
 
 class MyPageVC: UIViewController {
 
+    private let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .gray
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 40
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "iOSClimber"
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let levelLabel: UILabel = {
+        let label = UILabel()
+        label.text = "V6"
+        label.backgroundColor = .purple
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .white
+        label.layer.cornerRadius = 5
+        label.layer.masksToBounds = true
+        return label
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "체형: 177cm | 183cm"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .gray
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let editButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("프로필 편집", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        button.backgroundColor = .systemGray6
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let followCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "123"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let followingCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "456"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let followLabel: UILabel = {
+        let label = UILabel()
+        label.text = "팔로우"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let followingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "팔로잉"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let nameStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let profileStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private let totalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 32
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let followStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let followingStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        stackView.alignment = .center
+        return stackView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupLayout()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func setupLayout() {
+        [profileImage, profileStackView, totalStackView]
+            .forEach{ view.addSubview($0) }
+        
+        [nameStackView, infoLabel, editButton]
+            .forEach{ profileStackView.addArrangedSubview($0) }
+        
+        [nameLabel, levelLabel]
+            .forEach{ nameStackView.addArrangedSubview($0) }
+        
+        [followCountLabel, followLabel]
+            .forEach{ followStackView.addArrangedSubview($0) }
+        
+        [followingCountLabel, followingLabel]
+            .forEach{ followingStackView.addArrangedSubview($0) }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        [followStackView, followingStackView]
+            .forEach{ totalStackView.addArrangedSubview($0) }
+
+        profileImage.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(8)
+            $0.leading.equalToSuperview().inset(16)
+            $0.width.height.equalTo(80)
+        }
+        
+        profileStackView.snp.makeConstraints {
+            $0.leading.equalTo(profileImage.snp.trailing).offset(16)
+            $0.centerY.equalTo(profileImage.snp.centerY)
+            $0.height.equalTo(80)
+        }
+        
+        editButton.snp.makeConstraints {
+            $0.width.equalTo(120)
+        }
+        
+        totalStackView.snp.makeConstraints {
+            $0.leading.equalTo(profileStackView.snp.trailing).offset(32)
+            $0.centerY.equalTo(profileImage.snp.centerY)
+            $0.height.equalTo(80)
+        }
     }
-    */
-
 }
