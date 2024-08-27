7618882d52dccd6d610bee23b8dd2459baef0c9e
urdaeng <gni711@naver.com>
2024-08-27 10:38:05 +0900

    Feat: 검색바 추가

--- WeClimb/View/SearchVC.swift
+++ WeClimb/View/SearchVC.swift
@@ -10,12 +10,21 @@ import SnapKit
 
 class SearchVC: UIViewController {
     
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "암장이름, 지역"
+        return searchController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
         setNavigationBar()
+        setSearchController()
     }
     
     private func setNavigationBar() {
@@ -28,4 +37,18 @@ class SearchVC: UIViewController {
         navigationItem.leftBarButtonItem = leftBarButtonItem
     }
     
+    private func setSearchController() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false // 검색바 유지
+        definesPresentationContext = false // 검색바 유지
+    }
+    
+}
+
+extension SearchVC : UISearchResultsUpdating {
+    // MARK: - 검색 텍스트에 따라 검색 결과를 업데이트하는 메서드 - YJ
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchText = searchController.searchBar.text ?? ""
+        print("검색 텍스트: \(searchText)")
+    }
 }
