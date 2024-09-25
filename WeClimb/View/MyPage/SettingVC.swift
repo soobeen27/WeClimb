8c9311b3286ff80aa0e6e93103e2954eb1a77122
daesung <eogud8203@naver.com>
2024-09-26 07:39:59 +0900

    Rename: 위클팀 문의 -> 문의하기

--- WeClimb/View/MyPage/SettingVC.swift
+++ WeClimb/View/MyPage/SettingVC.swift
@@ -17,7 +17,7 @@ class SettingVC: UIViewController {
     
     private var datas: [SettingItem] = [
         //        SettingItem(section: .notifications, titles: [SettingNameSpace.notifications]),
-        SettingItem(section: .policy, titles: [SettingNameSpace.termsOfService, SettingNameSpace.privacyPolic]),
+        SettingItem(section: .policy, titles: [SettingNameSpace.termsOfService, SettingNameSpace.privacyPolic, SettingNameSpace.inquiry]),
         SettingItem(section: .account, titles: [SettingNameSpace.editProfile, SettingNameSpace.blackList, SettingNameSpace.logout, SettingNameSpace.accountRemove]),
     ]
     
