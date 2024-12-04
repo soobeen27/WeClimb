66816a1d3606eba57bf5feb729cf63211af26b70
Jang Soobeen <youdew66@gmail.com>
2024-12-04 16:41:43 +0900

    Refact: LoginFirebaseDataSource.  rx어려ㅑㅂ다

--- WeClimb/Data/Sources/DataSource/Login/LoginFirebaseDataSource.swift
+++ WeClimb/Data/Sources/DataSource/Login/LoginFirebaseDataSource.swift
@@ -20,86 +20,74 @@ class LoginFirebaseDataSourceImpl: LoginFirebaseDataSource {
     private let disposeBag = DisposeBag()
     
     func logIn(with credential: AuthCredential, loginType: LoginType) -> Single<LoginResult> {
-        return Single.create { [weak self] single in
-            guard let self else {
-                single(.failure(CommonError.selfNil))
-                return Disposables.create()
-            }
+        return Single<AuthDataResult>.create { single in
             Auth.auth().signIn(with: credential) { authResult, error in
                 if let error {
                     single(.failure(error))
                     return
                 }
-                self.getLoginResult(authResult: authResult)
-                    .subscribe(onSuccess: { loginResult in
-                        single(.success(loginResult))
-                    }, onFailure: { error in
-                        single(.failure(error))
-                    })
-                    .disposed(by: self.disposeBag)
+                guard let authResult else {
+                    single(.failure(LoginError.wrongAuthResult))
+                    return
+                }
+                single(.success(authResult))
             }
             return Disposables.create()
+        }.flatMap { [weak self] authResult in
+            guard let self else { return Single.error(CommonError.selfNil)}
+            return self.getLoginResult(authResult: authResult)
         }
     }
     
-    private func getLoginResult(authResult: AuthDataResult?) -> Single<LoginResult> {
-        return Single.create { [weak self] single in
-            guard let self else { 
-                single(.failure(CommonError.selfNil))
-                return Disposables.create()
-            }
-            guard let authResult else {
-                single(.failure(LoginError.wrongAuthResult))
-                return Disposables.create()
-            }
-            let user = authResult.user
-            let userRef = self.db.collection("users").document(user.uid)
-            userRef.getDocument { [weak self] document, error in
-                guard let self else {
-                    single(.failure(CommonError.selfNil))
-                    return
-                }
+    private func getLoginResult(authResult: AuthDataResult) -> Single<LoginResult> {
+        let user = authResult.user
+        let userRef = self.db.collection("users").document(user.uid)
+        return Single<DocumentSnapshot?>.create { single in
+            userRef.getDocument { document, error in
                 if let error {
                     single(.failure(error))
                     return
                 }
-                if let document, document.exists {
-                    single(.success(self.userNameExist(document: document)))
-                } else {
-                    self.createUser(ref: userRef)
-                        .subscribe(onSuccess: { loginResult in
-                            single(.success(loginResult))
-                        }, onFailure: { error in
-                            single(.failure(error))
-                        })
-                        .disposed(by: self.disposeBag)
-                }
+                single(.success(document))
             }
             return Disposables.create()
+        }
+        .flatMap { [weak self] document in
+            guard let self else { return .error(CommonError.selfNil)}
+            if let document, document.exists {
+                return .just(userNameExist(document: document))
+            } else {
+                return self.createUser(ref: userRef)
+            }
+            
         }
     }
     
     private func userNameExist(document: DocumentSnapshot) -> LoginResult {
-        if let userData = document.data(), let userName = userData["userName"] as? String {
-            return userNameEmpty(name: userName)
-        } else {
-            return .noName
-        }
-    }
-    
-    private func userNameEmpty(name: String) -> LoginResult {
-        if name.isEmpty {
-            return .noName
-        } else {
+        if let userName = document.data()?["userName"] as? String, !userName.isEmpty {
             return .login
+        } else {
+            return .noName
         }
     }
     
     private func createUser(ref: DocumentReference) -> Single<LoginResult> {
         return Single.create { single in
             do {
-                try ref.setData(from: User(userName: nil, profileImage: nil, registerationDate: Date(), lastModified: Date(), userRole: "user", armReach: nil, height: nil, posts: nil, comments: nil, followers: nil, following: nil, snsConsent: false, blackList: nil))
-                single(.success(.createAccount))
+                try ref.setData(from: User(userName: nil, profileImage: nil,
+                                           registerationDate: Date(), lastModified: Date(),
+                                           userRole: "user", armReach: nil,
+                                           height: nil, posts: nil,
+                                           comments: nil, followers: nil,
+                                           following: nil, snsConsent: false,
+                                           blackList: nil)) 
+                { error in
+                    if let error {
+                        single(.failure(error))
+                        return
+                    }
+                    single(.success(.createAccount))
+                }
             } catch {
                 single(.failure(error))
             }
