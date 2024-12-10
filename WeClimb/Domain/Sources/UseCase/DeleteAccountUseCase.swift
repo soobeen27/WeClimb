d78408ec1052fb6580169f54860e9dbf1e891714
urdaeng <gni711@naver.com>
2024-12-10 15:33:03 +0900

    Fix: 주석 삭제

--- WeClimb/Domain/Sources/UseCase/DeleteAccountUseCase.swift
+++ WeClimb/Domain/Sources/UseCase/DeleteAccountUseCase.swift
@@ -13,17 +13,17 @@ public protocol DeleteAccountUseCase {
 
 public struct DeleteAccountUseCaseImpl: DeleteAccountUseCase {
     public func execute() -> Completable {
-        print("메서드 호출")
+//        print("메서드 호출")
         
         return Completable.create { completable in
-            print("계정 삭제 시작.")
+//            print("계정 삭제 시작.")
             
             FirebaseManager.shared.userDelete { error in
                 if let error = error {
-                    print("사용자 삭제에 실패: \(error.localizedDescription)")
+//                    print("사용자 삭제에 실패: \(error.localizedDescription)")
                     completable(.error(error))
                 } else {
-                    print("사용자 삭제 완료")
+//                    print("사용자 삭제 완료")
                     completable(.completed)
                 }
             }
