7e0ef1e117fdb2474c1afcda320fa6bee462117d
daesung <eogud8203@naver.com>
2024-12-20 16:48:29 +0900

    Feat: 탭바 [0,2] 예외처리

--- WeClimb/Features/TabBar/View/TabBarVC.swift
+++ WeClimb/Features/TabBar/View/TabBarVC.swift
@@ -12,12 +12,29 @@ class TabBarVC: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
         setView()
-        print("혹시 들어오긴하나?")
     }
     
     private func setView() {
-        self.tabBar.tintColor = UIColor.black
+        updateTabBarTintColor(selectedIndex: self.selectedIndex)
+    }
+}
+
+extension TabBarVC: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        updateTabBarTintColor(selectedIndex: tabBarController.selectedIndex)
+    }
+    
+    private func updateTabBarTintColor(selectedIndex: Int) {
+        switch self.selectedIndex {
+        case 0, 2:
+            self.tabBar.tintColor = UIColor.white
+        case 1, 3, 4:
+            self.tabBar.tintColor = UIColor.black
+        default:
+            self.tabBar.tintColor = UIColor.black
+        }
         self.tabBar.unselectedItemTintColor = UIColor.gray
     }
 }
