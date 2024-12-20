1cd78f72fa7d56b5e0d99c17210bbd2b491d33fa
urdaeng <gni711@naver.com>
2024-12-20 15:46:43 +0900

    Fix: 빠진 임포트 추가

--- WeClimb/Shared/Components/Switch/PurpleSwitchNS.swift
+++ WeClimb/Shared/Components/Switch/PurpleSwitchNS.swift
@@ -5,6 +5,8 @@
 //  Created by 강유정 on 12/20/24.
 //
 
+import UIKit
+
 private enum PurpleSwitchNS {
     static let activeColor = UIColor(red: 160/255, green: 83/255, blue: 233/255, alpha: 1)
     static let disabledColor = UIColor(red: 223/255, green: 224/255, blue: 226/255, alpha: 1)
