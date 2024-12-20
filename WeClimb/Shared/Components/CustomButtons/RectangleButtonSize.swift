a97c2d39bf096ecab0103064cc4442893f8cd2c8
urdaeng <gni711@naver.com>
2024-12-20 13:33:19 +0900

    Refactor: 클래스명 및 변수명 변경

---
+++ WeClimb/Shared/Components/CustomButtons/RectangleButtonSize.swift
@@ -0,0 +1,84 @@
+//
+//  Untitled.swift
+//  WeClimb
+//
+//  Created by 강유정 on 12/20/24.
+//
+
+import UIKit
+
+enum RectangleButtonSize {
+    case large
+    case medium
+    case small
+    
+    var fontType: UIFont {
+        switch self {
+        case .large:
+            return UIFont.systemFont(ofSize: 16, weight: .semibold)
+        case .medium:
+            return UIFont.systemFont(ofSize: 14, weight: .medium)
+        case .small:
+            return UIFont.systemFont(ofSize: 12, weight: .light)
+        }
+    }
+    
+    var iconSize: CGFloat {
+        switch self {
+        case .large:
+            return 20
+        case .medium:
+            return 18
+        case .small:
+            return 16
+        }
+    }
+    
+    var width: CGFloat {
+        switch self {
+        case .large:
+            return 136
+        case .medium:
+            return 113
+        case .small:
+            return 93
+        }
+    }
+    
+    var height: CGFloat {
+        switch self {
+        case .large:
+            return 48
+        case .medium:
+            return 39
+        case .small:
+            return 30
+        }
+    }
+    
+    var padding: CGFloat {
+        switch self {
+        case .large:
+            return 28
+        case .medium:
+            return 20
+        case .small:
+            return 14
+        }
+    }
+    
+    var cornerRadius: CGFloat {
+        switch self {
+        case .large:
+            return 10
+        case .medium:
+            return 8
+        case .small:
+            return 6
+        }
+    }
+    
+    var interval: CGFloat {
+        return 6
+    }
+}
