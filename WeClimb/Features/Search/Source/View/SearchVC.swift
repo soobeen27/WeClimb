2c3e6682d802dcd8a87d317bedf1cf952198a3c8
urdaeng <gni711@naver.com>
2025-01-11 15:34:30 +0900

    Feat: 서치바의 커스텀 디자인 및 액션 추가

--- WeClimb/Features/Search/Source/View/SearchVC.swift
+++ WeClimb/Features/Search/Source/View/SearchVC.swift
@@ -25,48 +25,41 @@ struct Item {
     var height: Double?
 }
 
-class SearchVC: UIViewController, UISearchBarDelegate {
+class SearchVC: UIViewController, UISearchBarDelegate, UITextFieldDelegate {
     var coordinator: SearchCoordinator?
-    
+
     private let disposeBag = DisposeBag()
-    
+
     private var items = BehaviorRelay<[Item]>(value: [])
-    
+
     private let dummyItems: [Item] = [
         Item(type: .gym, name: "더 클라임", imageName: "gym_image1", location: "서울시 강남구", height: nil),
         Item(type: .user, name: "홍길동", imageName: "user_image1", location: nil, height: 180.0),
         Item(type: .gym, name: "9클라이밍", imageName: "gym_image2", location: "서울시 마포구", height: nil),
         Item(type: .user, name: "홍길동", imageName: "user_image2", location: nil, height: 175.0)
     ]
-    
-        private let searchBar: UISearchBar = {
-            let searchBar = UISearchBar()
-            searchBar.placeholder = "검색하기"
-    
-            searchBar.heightAnchor.constraint(equalToConstant: 46).isActive = true
-            searchBar.layer.cornerRadius = 8
-            searchBar.layer.borderWidth = 1
-            searchBar.layer.borderColor = UIColor.lineOpacityNormal.cgColor
-            searchBar.setImage(UIImage(named: "searchIcon"), for: .search, state: .normal)
-    
-            let searchTextField = searchBar.searchTextField
-            searchTextField.backgroundColor = .clear
-    
-            if let searchField = searchBar.value(forKey: "searchField") as? UITextField {
-                searchField.font = .customFont(style: .body2Medium)
-                
-                searchTextField.attributedPlaceholder = NSAttributedString(
-                       string: "검색하기",
-                       attributes: [
-                        .foregroundColor: UIColor.labelAssistive
-                       ]
-                   )
-    
-            }
-    
-            return searchBar
-        }()
-    
+
+    private let searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "검색하기"
+        textField.layer.cornerRadius = 8
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lineOpacityNormal.cgColor
+        textField.font = .customFont(style: .body2Medium)
+        textField.textColor = .black
+
+        let searchIcon = UIImageView(image: UIImage(named: "searchIcon"))
+        searchIcon.contentMode = .scaleAspectFit
+        let iconWrapper = UIView(frame: CGRect(x: 0, y: 0, width: 24 + 12 + 8, height: 24))
+        iconWrapper.addSubview(searchIcon)
+        searchIcon.frame.origin = CGPoint(x: 12, y: 1)
+
+        textField.leftView = iconWrapper
+        textField.leftViewMode = .always
+
+        return textField
+    }()
+
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "최근 방문"
@@ -75,7 +68,7 @@ class SearchVC: UIViewController, UISearchBarDelegate {
         label.textAlignment = .left
         return label
     }()
-    
+
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.separatorStyle = .none
@@ -84,46 +77,75 @@ class SearchVC: UIViewController, UISearchBarDelegate {
         tableView.rowHeight = 60
         return tableView
     }()
-    
+
+    private let smallCancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        let closeIcon = UIImage(named: "closeIcon.circle")?.withRenderingMode(.alwaysTemplate)
+        button.setImage(closeIcon, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .labelAssistive
+        button.alpha = 0
+        button.addTarget(self, action: #selector(didTapSmallCancelButton), for: .touchUpInside)
+        return button
+    }()
+
+    private let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        let closeIcon = UIImage(named: "closeIcon")?.withRenderingMode(.alwaysTemplate)
+        button.setImage(closeIcon, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .black
+        button.alpha = 0
+        button.addTarget(self, action: #selector(didTapCancelButton), for: .touchUpInside)
+        return button
+    }()
+
+    private let rightViewContainer: UIView = {
+        let view = UIView()
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
-        setNavigation()
+
         setLayout()
         bindTableView()
-        
-        items.accept(dummyItems)
-    }
-    
-    private func setNavigation() {
-        navigationItem.titleView = searchBar
 
-        searchBar.snp.makeConstraints {
-            $0.height.equalTo(46)
-            $0.top.equalTo(navigationController!.navigationBar).inset(16)
-            $0.leading.equalTo(navigationController!.navigationBar).offset(16)
-            $0.trailing.equalTo(navigationController!.navigationBar).offset(-16)
-            $0.bottom.equalTo(navigationController!.navigationBar)
-        }
+        items.accept(dummyItems)
+        searchTextField.delegate = self
+
     }
-    
+
     private func setLayout() {
-        [titleLabel, tableView].forEach { view.addSubview($0) }
-        
+        [searchTextField, titleLabel, tableView, cancelButton]
+            .forEach { view.addSubview($0)}
+
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(46)
+        }
+
         titleLabel.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.top.equalTo(searchTextField.snp.bottom).offset(8)
             $0.height.equalTo(56)
             $0.leading.equalToSuperview().inset(16)
         }
-        
+
         tableView.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom)
             $0.bottom.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview()
         }
+
+        cancelButton.snp.makeConstraints {
+            $0.width.height.equalTo(24)
+            $0.trailing.equalToSuperview().offset(48)
+            $0.centerY.equalTo(searchTextField)
+        }
     }
-    
+
     private func bindTableView() {
         items
             .bind(to: tableView.rx.items) { tableView, row, item in
@@ -141,8 +163,82 @@ class SearchVC: UIViewController, UISearchBarDelegate {
             }
             .disposed(by: disposeBag)
     }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        searchTextField.layer.borderWidth = 1
+        searchTextField.layer.borderColor = UIColor.fillSolidDarkBlack.cgColor
+
+        UIView.animate(withDuration: 0.3, animations: {
+            self.searchTextField.snp.updateConstraints {
+                $0.leading.equalToSuperview().offset(16)
+                $0.trailing.equalToSuperview().offset(-48)
+            }
+
+            self.cancelButton.alpha = 1
+            self.cancelButton.snp.updateConstraints {
+                $0.trailing.equalToSuperview().offset(-16)
+            }
+
+            self.view.layoutIfNeeded()
+        })
+    }
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        rightViewContainer.addSubview(smallCancelButton)
+
+
+        smallCancelButton.snp.makeConstraints {
+            $0.width.height.equalTo(16)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+        }
+
+        rightViewContainer.snp.makeConstraints {
+            $0.width.height.equalTo(30)
+        }
+
+        searchTextField.rightView = rightViewContainer
+        searchTextField.rightViewMode = .whileEditing
+
+        if let text = textField.text, !text.isEmpty {
+            smallCancelButton.alpha = 1
+        } else {
+            smallCancelButton.alpha = 0
+        }
+
+        return true
+    }
+
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func didTapCancelButton() {
+        searchTextField.text = ""
+        searchTextField.resignFirstResponder()
+        
+        searchTextField.layer.borderColor = UIColor.lineOpacityNormal.cgColor
+
+        UIView.animate(withDuration: 0.3) {
+            self.searchTextField.snp.updateConstraints {
+                $0.leading.equalToSuperview().offset(16)
+                $0.trailing.equalToSuperview().offset(-16)
+            }
+            
+            self.cancelButton.snp.updateConstraints {
+                $0.trailing.equalToSuperview().offset(16)
+            }
+            
+            self.cancelButton.alpha = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func didTapSmallCancelButton() {
+        searchTextField.text = ""
+        smallCancelButton.alpha = 0
+        searchTextField.rightView = nil
     }
 }
